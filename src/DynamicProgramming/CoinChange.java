cf3a6ef93e91ba46ece34d47a457e7ff33e71d0b
Man Manthan Bhardwaj <manthanbhardwaj5@gmail.com>
2020-06-22 12:25:34 +0530

    Wine Problem

--- src/DynamicProgramming/CoinChange.java
+++ src/DynamicProgramming/CoinChange.java
@@ -2,15 +2,18 @@ package DynamicProgramming;
 
 public class CoinChange {
     public static void main(String[] args){
-        printPattern(4);
+        int[] coins = {1, 2, 3};
+        System.out.println(coinChange(coins, 3, 4));
     }
-    public static void printPattern(int rows) {
 
-        for(int i = 1; i <= rows; ++i) {
-            for(int j = 1; j <= i; ++j) {
-                System.out.print("1 ");
-            }
-            System.out.println();
-        }
+    public static int coinChange(int[] coins, int num, int value) {
+        if (value == 0)
+            return 1;
+        if (value< 0)
+            return 0;
+        if (num <= 0 && value >= 0)
+            return 0;
+        return coinChange(coins, num - 1, value) +
+                coinChange(coins, num, value - coins[num-1]);
     }
 }
